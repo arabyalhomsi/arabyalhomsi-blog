2bc34c9ecad7478286743de5d02592fd33c2b7e2
Araby Alhomsi <araby.ami@gmail.com>
2015-12-05 22:01:53 +0200

    Remove normal Authentication files and new edits for JWT

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -46,6 +46,10 @@ class Handler extends ExceptionHandler
             $e = new NotFoundHttpException($e->getMessage(), $e);
         }
 
+        if ($e->getStatusCode() == 404) {
+            return response()->view('index');
+        }
+
         return parent::render($request, $e);
     }
 }
--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -26,9 +26,6 @@ class Kernel extends HttpKernel
      * @var array
      */
     protected $routeMiddleware = [
-        'auth' => \App\Http\Middleware\Authenticate::class,
-        'auth.basic' => \Illuminate\Auth\Middleware\AuthenticateWithBasicAuth::class,
-        'guest' => \App\Http\Middleware\RedirectIfAuthenticated::class,
         'jwt.auth' => \Tymon\JWTAuth\Middleware\GetUserFromToken::class,
         'jwt.refresh' => \Tymon\JWTAuth\Middleware\RefreshToken::class
     ];
